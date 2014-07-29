b34645203edbdb736bc6eff7519e9af97ab6dc3d
Alex Petersen <theoperatore@gmail.com>
2014-07-29 14:32:17 -0500

    hot fix for checking if currentRoom is null

--- core/Map.java
+++ core/Map.java
@@ -59,7 +59,7 @@ public class Map {
         for (int j = 0; j < height; j++)
         {
           Room currentRoom = map[i][j];
-          if (currentRoom.getPlayer() != null)
+          if (currentRoom != null && currentRoom.getPlayer() != null)
           {
             return currentRoom;
           }
