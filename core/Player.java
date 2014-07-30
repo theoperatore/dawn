adcc93b0d08a4f057a615f5b791b6fc2bbf5f85e
Alex Petersen <theoperatore@gmail.com>
2014-07-29 21:22:37 -0500

    Basic Player implementation

--- core/Player.java
+++ core/Player.java
@@ -1,22 +1,68 @@
 package core;
 
-import java.util.ArrayList;
-
 //
-// Represents the player -- inventory, current location...etc.
+// Basic Building Object
+// 
+// @author Alex Petersen <theoperatore@gmail.com>
 //
+public class WObject {
 
-public class Player extends WObject {
+    //both fields used to check for equality
+    private String name;
+    private String matchName;
+    private String description;
 
-    private ArrayList<WObject> inventory;
-
-    public Player() {
-        super("You!", "An adventurous person.");
-        inventory = new ArrayList<WObject>();
+    //constructors
+    public WObject() {
+        this.name = "obj";
+        this.matchName = "obj";
+        this.description = "";
     }
 
-    public void addToInventory(WObject o) {
-        inventory.add(o);
+    public WObject(String name) {
+        this.name = name;
+        this.matchName = name.toLowerCase().replace(' ', '_');
+        this.description = "";
     }
+
+    public WObject(String name, String desc) {
+        this.name = name;
+        this.matchName = name.toLowerCase().replace(' ', '_');
+        this.description = desc;
+    }
+
+    //accessors
+    public String getDisplayName() { return this.name; }
+    public String getMatchName() { return this.matchName; }
+    public String getDescription() { return this.description; }
+
+    //mutators
+    public void setName(String n)        { 
+        this.name = n;
+        this.matchName = n.toLowerCase().replace(' ', '_');
+    }
+    public void setDescription(String d) { this.description = d; }
+
+    //utilities
+    @Override
+    public String toString() {
+        return this.name + " : " + this.description; 
+    }
+
+    //
+    //Returns true iff both objects have the same name and 
+    //description
+    //
+    public boolean equals(WObject o) {
+        return (
+            this.matchName.equals(o.getMatchName()) && 
+            this.description.equals(o.getDescription())
+        );
+    }
+
+    public boolean equals(String name) {
+        return this.matchName.equals(name.toLowerCase().replace(' ','_'));
+    }
+
 
 }
