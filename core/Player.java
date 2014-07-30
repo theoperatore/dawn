4808b52031ca24f38d9cd4aa7251c717cc0c30bc
Alex Petersen <theoperatore@gmail.com>
2014-07-30 14:52:46 -0500

    Made player.java, Player.java again...

--- core/Player.java
+++ core/Player.java
@@ -1,67 +1,94 @@
 package core;
 
-//
-// Basic Building Object
-//
-// @author Alex Petersen <theoperatore@gmail.com>
-//
-public class WObject {
+import java.util.ArrayList;
+import constructs.Room;
 
-    //both fields used to check for equality
-    private String name;
-    private String matchName;
-    private String description;
-    private Room currentRoom;
+//
+// Represents the player.
+//
 
+public class Player extends WObject {
+
+    private ArrayList<WObject> inventory;
+    Room currentRoom;
+    private int score;
+
+    //
     //constructors
-    public WObject() {
-        this.name = "obj";
-        this.matchName = "obj";
-        this.description = "";
-    }
-
-    public WObject(String name) {
-        this.name = name;
-        this.matchName = name.toLowerCase().replace(' ', '_');
-        this.description = "";
-    }
-
-    public WObject(String name, String desc) {
-        this.name = name;
-        this.matchName = name.toLowerCase().replace(' ', '_');
-        this.description = desc;
-    }
-
-    //accessors
-    public String getDisplayName() { return this.name; }
-    public String getMatchName() { return this.matchName; }
-    public String getDescription() { return this.description; }
-
-    //mutators
-    public void setName(String n)        {
-        this.name = n;
-        this.matchName = n.toLowerCase().replace(' ', '_');
-    }
-    public void setDescription(String d) { this.description = d; }
-
-    //utilities
-    @Override
-    public String toString() {
-        return this.name + " : " + this.description;
-    }
-
     //
-    //Returns true iff both objects have the same name and
-    //description
-    //
-    public boolean equals(WObject o) {
-        return (
-            this.matchName.equals(o.getMatchName()) &&
-            this.description.equals(o.getDescription())
-        );
-
     public Player() {
-        super("You!", "An adventurous person.");
+        super(
+            "You", 
+            "An adventurous person; as beautiful on the inside as the outside."
+        );
         inventory = new ArrayList<WObject>();
+        score = 0;
+        currentRoom = null;
+    }
 
+    public Player(String name) {
+        super(
+            name,
+            "An adventurous person; as beautiful on the inside as the outside."
+        );
+        inventory = new ArrayList<WObject>();
+        score = 0;
+        currentRoom = null;
+    }
+
+    public Player(String name, String description) {
+        super(name, description);
+        inventory = new ArrayList<WObject>();
+        score = 0;
+        currentRoom = null;
+    }
+
+    //
+    //accessors
+    //
+    public ArrayList<WObject> getInventory() { return this.inventory; }
+    public int getScore() { return this.score; }
+    public Room getCurrentRoom() { return this.currentRoom; }
+
+    //check inventory for the item name
+    public boolean has(String name) {
+        for (int i = 0; i < this.inventory.size(); i++) {
+            if (this.inventory.get(i).equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Get an Item from the inventory; 
+    //@return -- item if exists, null otherwise
+    public WObject getItemFromInventory(String name) {
+        for (int i = 0; i < this.inventory.size(); i++) {
+            if (this.inventory.get(i).equals(name)) {
+                return this.inventory.get(i);
+            }
+        }
+        return null;
+    }
+
+    //
+    //mutators
+    //
+    public void setInventory(ArrayList<WObject> inv) { this.inventory = inv; }
+    public void addToInventory(WObject o) { inventory.add(o); }
+    public void clearInventory() { this.inventory.clear(); }
+    public void setScore(int s) { this.score = s; }
+    public void addToScore(int ds) { this.score += ds; }
+    public void setCurrentRoom(Room r) { this.currentRoom = r; }
+
+    //Attempts to remove the Item from the inventory and return it.
+    //@return -- the item removed, or null if not found.
+    public WObject removeItemFromInventory(String name) {
+        for (int i = 0; i < this.inventory.size(); i++) {
+            if (this.inventory.get(i).equals(name)) {
+                return this.inventory.remove(i);
+            }
+        }
+        return null;
+    }
 }
