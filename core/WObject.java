13f8e3a4d28a8855f6c827124079d8fd925eb314
Alex Petersen <theoperatore@gmail.com>
2014-07-29 21:17:56 -0500

    Updated Base WObject.java

--- core/WObject.java
+++ core/WObject.java
@@ -9,30 +9,38 @@ public class WObject {
 
     //both fields used to check for equality
     private String name;
+    private String matchName;
     private String description;
 
     //constructors
     public WObject() {
         this.name = "obj";
+        this.matchName = "obj";
         this.description = "";
     }
 
     public WObject(String name) {
         this.name = name;
+        this.matchName = name.toLowerCase().replace(' ', '_');
         this.description = "";
     }
 
     public WObject(String name, String desc) {
         this.name = name;
+        this.matchName = name.toLowerCase().replace(' ', '_');
         this.description = desc;
     }
 
     //accessors
-    public String getName()        { return this.name; }
+    public String getDisplayName() { return this.name; }
+    public String getMatchName() { return this.matchName; }
     public String getDescription() { return this.description; }
 
     //mutators
-    public void setName(String n)        { this.name = n; }
+    public void setName(String n)        { 
+        this.name = n;
+        this.matchName = n.toLowerCase().replace(' ', '_');
+    }
     public void setDescription(String d) { this.description = d; }
 
     //utilities
@@ -47,13 +55,13 @@ public class WObject {
     //
     public boolean equals(WObject o) {
         return (
-            this.name.toLowerCase().equals(o.getName().toLowerCase()) && 
+            this.matchName.equals(o.getMatchName()) && 
             this.description.equals(o.getDescription())
         );
     }
 
     public boolean equals(String name) {
-        return (this.name.toLowerCase().equals(name.toLowerCase()));
+        return this.matchName.equals(name.toLowerCase().replace(' ','_'));
     }
 
 
